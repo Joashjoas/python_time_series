31a24c66bf6d4cff1a41e953024dcab717154a39
Joashjoas <153771608+Joashjoas@users.noreply.github.com>
2025-08-25 20:34:54 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,18 @@
-
+#!/usr/bin/env python3  Line 1
+# -*- coding: utf-8 -*- Line 2
+# ----------------------------------------------------------------------------
+# Created By  : Joás   Line 3
+# Created Date: 25/08/2025
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""Isso é um cabeçalho, um cabeçalho é um bloco de comentários na parte superior do código, 
+que inclui o nome do arquivo, autor, data e alguns outros detalhes do arquivo e o conteúdo 
+desse arquivo. Seguem-se módulos embutidos importados e importações de terceiros. """  # Line 4
+# ---------------------------------------------------------------------------
+# Imports Line 5
+# ---------------------------------------------------------------------------
+from ... import ...  # Line 6
+# ---------------------------------------------------------------------------
+'''Uma docstring é um comentário explicando uma função ou parametros mais complexos
+onde um comentãrio simples é guardado para explicações menores, cabeçalhos contém as 
+docstrings de um projeto e pode ser feita de diferentes maneiras dependendo do desenvolvefor''' # Line7
